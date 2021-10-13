093b2ddb77e050aba12fbd706c4fbd5710d29582
Walter <wltrrpo@google.com>
2021-10-13 12:35:27 -0500

    models and path

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify, request, Response
 from flask_sqlalchemy import SQLAlchemy
-from src.main import main as main_blueprint
+from dna import dna_check
 
 
 app = Flask(__name__)
@@ -13,17 +13,61 @@ db.init_app(app)
 # db.drop_all()
 # db.create_all()
 
-# @app.route("/")
-# def index():
-#     return """
-#         <div>
-#             <h1>Welcome!</h1><br>
-#             <p>This site was released according to a test related with xmen and MELI.</p>
-#         </div>
-#     """
+class DNA_test(db.Model):
+    test_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    test_code = db.Column(db.String(255), primary_key=True)
+    test_result = db.Column(db.Boolean, default=False)
 
-app.register_blueprint(main_blueprint)
+    def __init__(self, test_code, test_result):
+        self.test_code = test_code
+        self.test_result = test_result
+
+
+def DNA_insert_sample(code, result):
+    try:
+        new_row = DNA_test(str(code), result)
+        db.session.add(new_row)
+        db.session.commit()
+    except:
+        pass
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return """
+        <div>
+            <h1>Welcome!</h1><br>
+            <p>This site was released according to a test related with xmen and MELI.</p>
+        </div>
+    """
+
+
+@app.route('/stats/', methods=['GET'])
+def stats():
+    try:
+        count_mutant = DNA_test.query.filter_by(test_result=1).count()
+        count_human = DNA_test.query.filter_by(test_result=0).count()
+        ratio = count_mutant / (0 if count_human + count_mutant == 0 else count_human + count_mutant)
+    except:
+        count_mutant = 0
+        count_human = 0
+        ratio = 0
+    return jsonify({'count_mutant_dna':count_mutant, 'count_human_dna': count_human, 'ratio':ratio})
+
+
+@app.route('/mutant/', methods=['POST'])
+def mutant():
+    try:
+        dna_code = request.get_json(force=True)['dna']
+        dna_result = dna_check(dna_code)
+        DNA_insert_sample(dna_code, dna_result)
+        if dna_result:
+            return Response(status=200)
+        else:
+            return Response(status=403)
+    except:
+        return Response(status=403)
 
 if __name__ == "__main__":
-    app.run()
-    # app.run(threaded=True)
+    # app.run()
+    app.run(threaded=True)
